766c7398fbe3094e5ecf0b3c2948085af2bb11fc
renangasantos-a11y <renan_ga_santos@estudante.sesisenai.org.br>
2026-03-13 19:56:44 +0000

    update

--- atividade 1/programa4.py
+++ atividade 1/programa4.py
@@ -1,7 +1,14 @@
-nome = input("Insira o nome  do aluno :")
-nota1 = float(input("Insira o primerira nota :"))
-nota2 = float(input("Insira o segundo nota :"))
-media = (nota1 + nota2) / 2
+contador = 1
+soma_notas = 0
+
+while contador <=4:
+    nota = float(input(f"digite a nota do {contador} bimestre"))
+    if nota < 0 or nota > 10:
+        print("nota invalida. a nota deve estar entre 0 e 10")
+        continue
+    contador += 1
+    soma_notas += nota
+media =  soma_notas /4
 print("Media das notas é: ", media) 
 if media >= 7:
     print("O aluno esta aprovado :")
